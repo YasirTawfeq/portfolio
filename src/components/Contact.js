78f1bfe26cc2d6793a95ab1631756987602c28a3
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-08-07 11:04:20 +0300

    making contact form work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Contact() {
+
+   const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_chtwtga', 'template_h9a8k4s', e.target, 'nZlkeEEKDfoKQie_1')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+
+   const notify = () =>{toast.success('The message was successfully', {
+                       position: "top-center",
+                       autoClose: 5000,
+                       hideProgressBar: false,
+                       closeOnClick: true,
+                       pauseOnHover: true,
+                       draggable: true,
+                       progress: undefined,
+                       });};
+
   return (<>
      <svg className="w-full m-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#383838" fill-opacity="1" d="M0,128L720,256L1440,96L1440,320L720,320L0,320Z"></path></svg>
       <div className="bg-[#383838]" >
@@ -8,14 +34,17 @@ function Contact() {
          <p className=" text-gray-100 text-center pt-8 bg-[#383838] ">Have a question or want to work together?</p>
          <div className="bg-[#383838] m-0 flex justify-center">
     
-            <form className="flex flex-col justify-center w-96 pb-8 pt-2 lg:m-0 mx-4 " >
-                <input className=" bg-gray-700 p-2 text-gray-100  mt-1 " type="text" placeholder="Name" />
-                <input className=" bg-gray-700 p-2 text-gray-100  mt-1 " type="email" placeholder="Email" />
-                <textarea className=" bg-gray-700 p-2 text-gray-100  mt-1 " rows="5" cols="33" placeholder="Write your message here..." ></textarea>
-                <button className=" bg-gray-700 p-2 mt-1 text-gray-100 hover:bg-gray-500 transform duration-300 "  type="submit"  >Send</button>  
+            <form onSubmit={sendEmail} className="flex flex-col justify-center w-96 pb-8 pt-2 lg:m-0 mx-4 "  >
+                <input name="user_name" className=" bg-gray-700 p-2 text-gray-100  mt-1 " type="text" placeholder="Name" />
+                <input name="user_email" className=" bg-gray-700 p-2 text-gray-100  mt-1 " type="email" placeholder="Email" />
+                <textarea name="message" className=" bg-gray-700 p-2 text-gray-100  mt-1 " rows="5" cols="33" placeholder="Write your message here..." ></textarea>
+                <button className=" bg-gray-700 p-2 mt-1 text-gray-100 hover:bg-gray-500 transform duration-300 " onClick={notify} type="submit"  >Send</button>  
             </form>
 
         </div>
+        <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable
+pauseOnHover
+/>
     </div>
     </>
   )
