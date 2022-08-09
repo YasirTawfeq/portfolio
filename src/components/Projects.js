87792761cdce4b93cefcf4470853caf541153f78
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-08-09 14:27:42 +0300

    adding yona projrct

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,11 +21,11 @@ function Projects() {
         </div>
         
         <div className=" h-64 relative group transform  ">
-            <img className="h-full w-full group-hover:opacity-30 duration-300 " src="https://images8.alphacoders.com/712/712496.jpg" alt="project1" />
+            <img className="h-full w-full group-hover:opacity-30 duration-300 " src={require("../imgs/yona.png")} alt="project1" />
             <div className=" flex flex-col invisible group-hover:visible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full items-center justify-center ">
-              <p className="text-[#FF1E00] text-2xl font-bold " >KURD<small>ANIME</small></p>
-              <small className="text-[#FF1E00] font-bold " >Tailwind / React</small>
-               <a href="https://kurdanime.netlify.app/" target="_blank" rel="noreferrer" className="flex hover:bg-orange-500 duration-100 bg-[#FF1E00] p-1 px-8 font-semibold text-gray-200 text-lg rounded-sm mt-8 ">
+              <p className="text-[#FF1E00] text-2xl font-bold " >Yona</p>
+              <small className="text-[#FF1E00] font-bold " >MUI / React</small>
+               <a href="https://yonagallary.netlify.app/" target="_blank" rel="noreferrer" className="flex hover:bg-orange-500 duration-100 bg-[#FF1E00] p-1 px-8 font-semibold text-gray-200 text-lg rounded-sm mt-8 ">
                 <div className="px-1" ><RemoveRedEyeIcon sx={{ fontSize: 25 ,color:"white" }} /></div>
                 <span className="mt-0.5" > View</span>
               </a>
